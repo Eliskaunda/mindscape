60021e4ab5b404b720262c93ba639a636f04870f
dennis-dentrix <denkyusya@gmail.com>
2023-11-21 19:13:49 +0300

    resource

--- src/features/Resources/Documents.jsx
+++ src/features/Resources/Documents.jsx
@@ -1,8 +1,14 @@
 import { useQuery } from "@tanstack/react-query";
 import { Heart, Search } from "react-bootstrap-icons";
 import { getResources } from "../../services/resource";
+import Loader from "./../../ui/Loader";
 
 export default function Documents() {
+  const { data: document, isLoading } = useQuery({
+    queryKey: ["resources"],
+    queryFn: getResources,
+  });
+  if (isLoading) return <Loader />;
   return (
     <div className="space-y-4 mx-auto ">
       <h1>Documents</h1>
@@ -22,27 +28,25 @@ export default function Documents() {
         </form>
       </div>
       <div className="flex  flex-wrap gap-6 md:justify-normal mt-4">
+        {document.map((doc) => (
+          <DocumentCard key={doc.id} doc={doc} />
+        ))}
+
+        {/* <DocumentCard />
         <DocumentCard />
-        <DocumentCard />
-        <DocumentCard />
-        <DocumentCard />
+        <DocumentCard /> */}
       </div>
     </div>
   );
 }
 
-function DocumentCard() {
-  const { data: document, isLoading } = useQuery({
-    queryKey: ["resources"],
-    queryFn: getResources,
-  });
+function DocumentCard({ doc }) {
+  const { title, description } = doc;
   return (
     <div className="bg-white shadow-lg rounded-md px-3 py-4 flex gap-4 w-60 md:w-[18rem]">
       <div className="space-y-6">
-        <h1 className="font-bold text-black">Foods to improve brain health</h1>
-        <p className="font-light text-sm">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
+        <h1 className="font-bold text-black">{title}</h1>
+        <p className="font-light text-sm">{description}</p>
       </div>
       <div className="flex justify-center items-baseline gap-1 text-lightBlue">
         {/* <ThreeDotsVertical /> */}
